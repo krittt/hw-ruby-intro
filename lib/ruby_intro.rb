c1b51fec3a971c1034fc3cde62c77cd9754f0aab
Krit Gupta <kritgupta@tamu.edu>
2017-09-16 00:58:28 +0000

    Check

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,107 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.inject(0,:+)
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.max(2).inject(0, :+)
 end
 
-def sum_to_n? arr, n
-  # YOUR CODE HERE
+def sum_to_n?(arr, n)
+  if arr.length <= 1
+    return false
+  end
+  
+target = 0 
+i = 0
+sum = n
+
+  for i in arr do
+    target = sum - i
+    if arr.include?(target)
+      return true
+    else
+      return false
+    end
+  end  
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return 'Hello, ' + name
 end
 
-def starts_with_consonant? s
-  # YOUR CODE HERE
+def starts_with_consonant?(s)
+  
+  if s == ""
+    return false
+  end
+  
+  s = s.downcase
+  vowels = ['a','e','i','o','u']
+  
+  if s =~ /[[A-Za-z]]/
+    
+    if vowels.include?(s[0])
+      return true
+    else
+      return false
+    end
+    return false
+  end
+  
+  return false
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4?(s)
+  
+  if s[-1,2] == "00"
+    return true
+  else
+    return false
+  end
+  
+  s.split("").each do |i|
+    if i != '0' || i != '1'
+      return false
+    end
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    @isbn = isbn
+    @price = price
+    raise ArgumentError.new("ISBN Number is empty") if isbn == ""
+    raise ArgumentError.new("Price is zero or less than zero") if price <= 0
+  end 
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(str)
+   @isbn = str
+  end
+ 
+  def price=(price)
+   @price = price
+  end
+ 
+  def price_as_string(price)
+   var = price.to_f
+   flt = var.round(2)
+   str = flt.to_s
+   "$"+str
+  end
+    
 end
